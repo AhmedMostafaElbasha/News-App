647d070d856fa5385c5ead5db0c6bcc9f62871db
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2020-07-31 12:40:43 +0200

    Modified the Health Screen UI Design

--- news_app/lib/ui/screens/health_screen.dart
+++ news_app/lib/ui/screens/health_screen.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/data/news_provider.dart';
+import 'package:newsapp/ui/widgets/loading_state.dart';
+import 'package:newsapp/ui/widgets/news_list.dart';
+import 'package:provider/provider.dart';
 
-class HealthScreen extends StatelessWidget {
+class HealthScreen extends StatefulWidget {
   static String routeName = '/health_screen';
 
   @override
+  _HealthScreenState createState() => _HealthScreenState();
+}
+
+class _HealthScreenState extends State<HealthScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<NewsProvider>(context)
+          .fetchAndSetNewsItems('health')
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Health Screen',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    double width = MediaQuery.of(context).size.width;
+
+    return _isLoading
+        ? LoadingState()
+        : NewsList(
+            width: width,
+            categoryName: 'health',
+          );
   }
 }
