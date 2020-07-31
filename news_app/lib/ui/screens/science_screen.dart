baf8e0b4cad42d010de38f506dfd2275e8edda5a
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2020-07-31 12:45:17 +0200

    Modified the UI Design of Science Screen

--- news_app/lib/ui/screens/science_screen.dart
+++ news_app/lib/ui/screens/science_screen.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/data/news_provider.dart';
+import 'package:newsapp/ui/widgets/loading_state.dart';
+import 'package:newsapp/ui/widgets/news_list.dart';
+import 'package:provider/provider.dart';
 
-class ScienceScreen extends StatelessWidget {
+class ScienceScreen extends StatefulWidget {
   static String routeName = '/science_screen';
 
   @override
+  _ScienceScreenState createState() => _ScienceScreenState();
+}
+
+class _ScienceScreenState extends State<ScienceScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<NewsProvider>(context)
+          .fetchAndSetNewsItems('science')
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Science Screen',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    double width = MediaQuery.of(context).size.width;
+
+    return _isLoading
+        ? LoadingState()
+        : NewsList(
+            width: width,
+            categoryName: 'science',
+          );
   }
 }
