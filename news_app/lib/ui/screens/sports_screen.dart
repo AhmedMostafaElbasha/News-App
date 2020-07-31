a2e1f34197c3b1f37d27e079f48cadae1a000f48
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2020-07-31 12:49:42 +0200

    Modified the UI design of Sports Screen

--- news_app/lib/ui/screens/sports_screen.dart
+++ news_app/lib/ui/screens/sports_screen.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/data/news_provider.dart';
+import 'package:newsapp/ui/widgets/loading_state.dart';
+import 'package:newsapp/ui/widgets/news_list.dart';
+import 'package:provider/provider.dart';
 
-class SportsScreen extends StatelessWidget {
+class SportsScreen extends StatefulWidget {
   static String routeName = '/sports_screen';
+
+  @override
+  _SportsScreenState createState() => _SportsScreenState();
+}
+
+class _SportsScreenState extends State<SportsScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<NewsProvider>(context)
+          .fetchAndSetNewsItems('sports')
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Sports Screen',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    double width = MediaQuery.of(context).size.width;
+
+    return _isLoading
+        ? LoadingState()
+        : NewsList(
+            width: width,
+            categoryName: 'sports',
+          );
   }
 }
