9e7b01c4df05885d0a1b82766da2d8ab3631513d
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2020-07-31 14:00:46 +0200

    Modified the UI design of Technology Screen

--- news_app/lib/ui/screens/technology_screen.dart
+++ news_app/lib/ui/screens/technology_screen.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/data/news_provider.dart';
+import 'package:newsapp/ui/widgets/loading_state.dart';
+import 'package:newsapp/ui/widgets/news_list.dart';
+import 'package:provider/provider.dart';
 
-class TechnologyScreen extends StatelessWidget {
+class TechnologyScreen extends StatefulWidget {
   static String routeName = '/technology_screen';
 
   @override
+  _TechnologyScreenState createState() => _TechnologyScreenState();
+}
+
+class _TechnologyScreenState extends State<TechnologyScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<NewsProvider>(context)
+          .fetchAndSetNewsItems('technology')
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Technology Screen',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    double width = MediaQuery.of(context).size.width;
+
+    return _isLoading
+        ? LoadingState()
+        : NewsList(
+            width: width,
+            categoryName: 'technology',
+          );
   }
 }
